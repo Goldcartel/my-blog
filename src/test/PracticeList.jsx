fba1aa0be62a7864732c8dde039c90b619370093
name <soper9114@naver.com>
2023-03-24 19:38:09 +0900

    id

--- src/test/PracticeList.jsx
+++ src/test/PracticeList.jsx
@@ -3,16 +3,19 @@ import Practice from "./Practice";
 
 const array = [
     {
+        id:1,
         name:"손흥민",
         team:"토트넘",
         logo:"tot.png",
     },
     {
+        id:2,
         name:"김민재",
         team:"나폴리",
         logo:"nap.png",
     },
     {
+        id:3,
         name:"황희찬",
         team:"울버햄튼",
         logo:"wol.png",
@@ -23,7 +26,7 @@ function TestList() {
     return(
         <div>
             {
-                array.map((arr)=><Practice name={arr.name} team ={arr.team} logo={arr.logo}/>)
+                array.map((arr)=><Practice key={arr.id} name={arr.name} team ={arr.team} logo={arr.logo}/>)
             }
         </div>
     )
