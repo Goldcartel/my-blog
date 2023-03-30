24ceecdd6874fed0cb07e30b63e06298a0e9e696
name <soper9114@naver.com>
2023-03-30 18:00:00 +0900

    todolist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import { useState } from 'react';
-import './App.css';
-import SignUp from './chapter_11/SignUp';
-import BoxClass  from './lecture/BoxClass';
+import React,{useState} from 'react';
+import './App.css'; 
+import TodoBoard from './lecture/TodoBoard';
 
 function App() {
-  return(
-    <BoxClass/>
+  const [inputValue,setInputValue]=useState('');
+  const [todoList,setTodoList]=useState([ ]);
+  const delItem=()=> {
+    console.log(todoList)
+    setTodoList((todoList)=> todoList.slice(0,-1))
+    console.log(todoList)
+  }
+  const addItem=( )=>{
+    console.log('inputValue 여기에!',inputValue);
+    setTodoList([...todoList,inputValue]);
+  }
+  return (
+    <div className="App">
+      <input value={inputValue} type='text' onChange={(event)=>setInputValue(event.target.value)}/>
+      <button onClick={addItem}>추가!</button>
+      <TodoBoard todoList={todoList}/>
+      <button onClick={delItem}>삭제</button>
+    </div>
   );
 }
 
